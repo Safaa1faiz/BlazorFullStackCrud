56777f8bf39a7ff4c2188429741be8e3ae7078a2
hussein bouik <116066389+husseinbouik@users.noreply.github.com>
2024-09-30 15:21:43 +0100

    Add project files.

---
+++ Client/Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorFullStackCrud.Client.Services.SuperHeroService
+{
+    public interface ISuperHeroService
+    {
+        List<SuperHero> Heroes { get; set; }
+
+        List<Comic> Comics { get; set; }
+
+        Task GetComics();
+        Task GetSuperHeroes();
+
+        Task<SuperHero> GetSingleHero(int id);
+        Task<List<SuperHero>> GetDbHeroes();
+        Task CreateSuperHero(SuperHero hero);
+		Task UpdateSuperHero(SuperHero hero, int id);
+        Task DeleteSuperHero(int id);
+	}
+}
